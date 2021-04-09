9249fb8c67de847385c9b5b95adeff2a3f5654eb
Yaxuan Li <yaxuanl@andrew.cmu.edu>
2021-04-09 23:40:57 +0800

    try to do the delete button

--- scripts.js
+++ scripts.js
@@ -1,22 +1,21 @@
 function viewNumbers(){
   
-    if (localStorage.getItem('cartNumbers')==null){
-      
-      localStorage.setItem('cartNumbers',0);
-      
-      document.getElementById('item_length').innerText = 0;
-    }
-    else{
+    if (localStorage.getItem('cartNumbers')!=null){
       let productNumbers = localStorage.getItem('cartNumbers');
 
-      productNumbers = parseInt(productNumbers);
-        
       document.getElementById('item_length').innerText = productNumbers;
-
     }
-    
-  
-    LoadPic();
+    else{
+      localStorage.setItem('cartNumbers','0');
+      
+      document.getElementById('item_length').innerText = '0';
+    }
+ 
+    let productContainer = document.querySelector(".original_container");
+
+    if(productContainer ){
+      LoadPic();
+    }
 }
 
 
@@ -24,7 +23,8 @@ function updateNumbers(){
     let productNumbers = localStorage.getItem('cartNumbers');
     if (productNumbers!=null){
         productNumbers = parseInt(productNumbers);
-        localStorage.setItem('cartNumbers', productNumbers + 1);   
+        productNumbers = productNumbers + 1;
+        localStorage.setItem('cartNumbers', productNumbers.toString());   
         }
     else{
         localStorage.setItem('cartNumbers',1);
@@ -175,73 +175,73 @@ function LoadPic(){
   if(localStorage.getItem('size')=='1'){
     
     if(localStorage.getItem('glazing')=='None'){
-      document.getElementById("demo").setAttribute("src", "images/none-original.JPG");
+      document.getElementById("demo").setAttribute("src", "images/none1.jpg");
       localStorage.setItem('index','0');
     }
     else if(localStorage.getItem('glazing')=='Sugar_milk'){
-      document.getElementById("demo").setAttribute("src", "images/sugar-milk-original.JPG");
+      document.getElementById("demo").setAttribute("src", "images/sugar1.jpg");
       localStorage.setItem('index','1');
     }
     else if(localStorage.getItem('glazing')=='Vanilla_milk'){
-      document.getElementById("demo").setAttribute("src", "images/vanilla-milk-original.JPG");
+      document.getElementById("demo").setAttribute("src", "images/vanilla1.jpg");
       localStorage.setItem('index','2');
     }
     else if(localStorage.getItem('glazing')=='Double_chocolate'){
-      document.getElementById("demo").setAttribute("src", "images/chocolate-original.PNG");
+      document.getElementById("demo").setAttribute("src", "images/chocolate1.jpg");
       localStorage.setItem('index','3');
     }
   }
   else if(localStorage.getItem('size')=='3'){
     if(localStorage.getItem('glazing')=='None'){
-      document.getElementById("demo").setAttribute("src", "images/none-original-3.JPG");
+      document.getElementById("demo").setAttribute("src", "images/none3.jpg");
       localStorage.setItem('index','4');
     }
     else if(localStorage.getItem('glazing')=='Sugar_milk'){
-      document.getElementById("demo").setAttribute("src", "images/sugar-milk-original-3.JPG");
+      document.getElementById("demo").setAttribute("src", "images/sugar3.jpg");
       localStorage.setItem('index','5');
     }
     else if(localStorage.getItem('glazing')=='Vanilla_milk'){
-      document.getElementById("demo").setAttribute("src", "images/vanilla-milk-original-3.JPG");
+      document.getElementById("demo").setAttribute("src", "images/vanilla3.jpg");
       localStorage.setItem('index','6');
     }
     else if(localStorage.getItem('glazing')=='Double_chocolate'){
-      document.getElementById("demo").setAttribute("src", "images/chocolate-original-3.JPG");
+      document.getElementById("demo").setAttribute("src", "images/chocolate3.jpg");
       localStorage.setItem('index','7');
     }
   }
   else if (localStorage.getItem('size')=='6'){
     if(localStorage.getItem('glazing')=='None'){
-      document.getElementById("demo").setAttribute("src", "images/none-original-6.JPG");
+      document.getElementById("demo").setAttribute("src", "images/none6.jpg");
       localStorage.setItem('index','8');
     }
     else if(localStorage.getItem('glazing')=='Sugar_milk'){
-      document.getElementById("demo").setAttribute("src", "images/sugar-milk-original-6.JPG");
+      document.getElementById("demo").setAttribute("src", "images/sugar6.jpg");
       localStorage.setItem('index','9');
     }
     else if(localStorage.getItem('glazing')=='Vanilla_milk'){
-      document.getElementById("demo").setAttribute("src", "images/vanilla-milk-original-6.JPG");
+      document.getElementById("demo").setAttribute("src", "images/vanilla6.jpg");
       localStorage.setItem('index','10');
     }
     else if(localStorage.getItem('glazing')=='Double_chocolate'){
-      document.getElementById("demo").setAttribute("src", "images/chocolate-original-6.JPG");
+      document.getElementById("demo").setAttribute("src", "images/chocolate6.jpg");
       localStorage.setItem('index','11');
     }
   }
   else if (localStorage.getItem('size')=='12'){
     if(localStorage.getItem('glazing')=='None'){
-      document.getElementById("demo").setAttribute("src", "images/none-original-12.JPG");
+      document.getElementById("demo").setAttribute("src", "images/none12.jpg");
       localStorage.setItem('index','12');
     }
     else if(localStorage.getItem('glazing')=='Sugar_milk'){
-      document.getElementById("demo").setAttribute("src", "images/sugar-milk-original-12.JPG");
+      document.getElementById("demo").setAttribute("src", "images/sugar12.jpg");
       localStorage.setItem('index','13');
     }
     else if(localStorage.getItem('glazing')=='Vanilla_milk'){
-      document.getElementById("demo").setAttribute("src", "images/vanilla-milk-original-12.JPG");
+      document.getElementById("demo").setAttribute("src", "images/vanilla12.jpg");
       localStorage.setItem('index','14');
     }
     else if(localStorage.getItem('glazing')=='Double_chocolate'){
-      document.getElementById("demo").setAttribute("src", "images/chocolate-original-12.JPG");
+      document.getElementById("demo").setAttribute("src", "images/chocolate12.jpg");
       localStorage.setItem('index','15');
     }
   }
@@ -271,22 +271,60 @@ function addToCart(product){
   localStorage.setItem('productsInCart', JSON.stringify(cartItems));
 }
 
+function deleteButtons(){
+  console.log("Adfas d");
+  let deleteButtons = document.querySelectorAll('.delete');
+  let productName;
+  let productNumbers = localStorage.getItem('cartNumbers');
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+  let cartCost = localStorage.getItem('totalCost');
 
+  for(let i = 0; i < deleteButtons.length; i++){
+    deleteButtons[i].addEventListener('click',() => {
+      productName = deleteButtons[i].parentElement.textContext.trim();
+      console.log("product",productName);
+      localStorage.setItem('cartNumbers',productNumbers - cartItems[productName].inCart);
 
-function display(){
-  bucket1 = localStorage.getItem("cart_size");
-  bucket2 = localStorage.getItem("cart_glazing");
-  console.log("length",bucket1.length);
-  console.log("bucket1",bucket1);
-  console.log("bucket2",bucket2);
-  for (i = 0; i < bucket1.length; i++) {
-    console.log("inside of setitems function");
-    console.log("the size is ", bucket1[i]);
-    console.log("the glazing is ", bucket2[i]);
+      delete cartItems[productName];
+      localStorage.setItem('productsInCart', JSON.stringify(cartItems)); 
 
+      displayCart();
+      onLoadviewNumbers();
+    });
   }
 }
 
+
+// learn from https://www.youtube.com/watch?v=IY5UN82FZ2Q
+function displayCart(){
+  
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let productContainer = document.querySelector(".products-container");
+
+  console.log(cartItems);
+  if( cartItems && productContainer ){
+    productContainer.innerHTML = '';
+    Object.values(cartItems).map(item => {
+      console.log("this is the itesm",item);
+      productContainer.innerHTML +=`
+      <div class = "product">
+        <img src = "images/${item.tag}.jpg" class="product_listing_pic">
+        <br/>
+        <span>Name: ${item.name}</span>
+      </div>
+      <div class = "price">Price: ${item.price}</div>
+      <div class = "quantity"><span>Size: ${item.inCart}</span></div>
+      
+      <button class = "delete">delete</button>  
+      `;
+      
+    });
+
+  }
+  deleteButtons();
+}
 
 function changeSize1(){
   localStorage.setItem('size', '1');
@@ -328,3 +366,4 @@ function changeGlazing4(){
   location.reload();
 }
 
+displayCart()
